7c2a197fabe26dce807d95a58ee6d40f599fac3a
Robert Gleason <l2ob122@gmail.com>
2025-10-27 20:39:48 -0700

    feat: implement mailer functionality and integrate welcome email on user registration

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"epiqreel.robertgleason.ca/internal/data"
+	"epiqreel.robertgleason.ca/internal/mailer"
 	_ "github.com/lib/pq"
 )
 
@@ -29,12 +30,21 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *slog.Logger
 	models data.Models
+	mailer *mailer.Mailer
 }
 
 func main() {
@@ -54,6 +64,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 2525, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "3c690680555e18", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "a6576310625445", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "EpiqReel <no-reply@epiqreel.robertgleason.ca>", "SMTP sender")
+
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
@@ -68,10 +84,17 @@ func main() {
 
 	logger.Info("database connection pool established")
 
+	mailer, err := mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
 	app := &application{
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer,
 	}
 
 	err = app.serve()
